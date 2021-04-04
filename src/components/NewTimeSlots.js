f6ddead4e7e8aaa96395e29b86dd8ed116316876
Abedelhak <abdelhak@groupado.com>
2021-04-04 14:21:25 +0100

    update newTimeSlots component

--- src/components/NewTimeSlots.js
+++ src/components/NewTimeSlots.js
@@ -1,17 +1,54 @@
 import React, { useState } from 'react'
-import TimeRangePicker from '@wojtekmaj/react-timerange-picker'
+import moment from "moment"
+import TextField from '@material-ui/core/TextField';
 
-
-const NewTimeSlots = ({ onSubmit }) => {
+const NewTimeSlots = ({ date, onSubmit }) => {
     const [timeRange, setTimeRange] = useState(['10:00', '11:00']);
+    const readableDate = moment(date).format('MMMM Do')
 
     return (
         <div className='new-time-slot'>
-            <TimeRangePicker
-                onChange={setTimeRange}
-                value={timeRange}
-            />
-            <button className='primary-btn' onClick={() => onSubmit(timeRange)}> Submit </button>
+            <div className='container ptb-100'>
+                <h2>Add New Time Slot in {readableDate}</h2>
+                <TextField
+                    id="start-time"
+                    className='mp-8'
+                    label="Start Time"
+                    type="time"
+                    value={timeRange[0]}
+                    onChange={event => setTimeRange([event.target.value, timeRange[1]])}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{
+                        step: 300, // 5 min
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="end-time"
+                    className='mp-8'
+                    label="End Time"
+                    type="time"
+                    value={timeRange[1]}
+                    onChange={event => setTimeRange([timeRange[0], event.target.value])}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{
+                        step: 300, // 5 min
+                    }}
+                    variant="outlined"
+                />
+                <button 
+                    className='primary-btn' 
+                    onClick={() => {
+                        onSubmit({date, start: timeRange[0], end: timeRange[1]})
+                    }}
+                    > 
+                        Submit 
+                    </button>
+            </div>
         </div>
     )
 }
