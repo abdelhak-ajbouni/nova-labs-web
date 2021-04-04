41b0606ec00615c180e83133b97dbd3e6a334784
Abedelhak <abdelhak@groupado.com>
2021-04-04 14:23:00 +0100

    add react-query

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import reportWebVitals from './reportWebVitals';
+
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
