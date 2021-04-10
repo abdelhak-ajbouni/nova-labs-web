dbe1f71ece4b4fdbba770b330cbb522b6853087b
Abedelhak <abdelhak@groupado.com>
2021-04-10 22:11:30 +0100

    add request endpoints

--- src/libs/apis.js
+++ src/libs/apis.js
@@ -4,6 +4,10 @@ const addTimeSlotToSeller = (sellerId, body) => {
    return axios.post(`sellers/${sellerId}/timeslots`, body)
 }
 
+const addSeller = (body) => {
+  return axios.post(`sellers/`, body)
+}
+
 const getSellerById = (sellerId, date) => {
     return axios.get(`sellers/${sellerId}`, {
         params: {
@@ -12,7 +16,18 @@ const getSellerById = (sellerId, date) => {
       })
 }
 
+const getRequests = (sellerId, timeslotId) => {
+  return axios.get(`sellers/${sellerId}/timeslots/${timeslotId}/requests`)
+}
+
+const updateRequest = (sellerId, timeslotId, requestId, body) => {
+  return axios.put(`sellers/${sellerId}/timeslots/${timeslotId}/requests/${requestId}`, body)
+}
+
 export { 
     addTimeSlotToSeller,
-    getSellerById
+    getSellerById,
+    getRequests,
+    addSeller,  
+    updateRequest
 }
