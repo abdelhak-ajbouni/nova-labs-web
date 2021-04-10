d0193fe26050107ed88d5b6da66dd1a81fcccb6c
Abedelhak <abdelhak@groupado.com>
2021-04-10 22:11:57 +0100

    finish requests ui

--- src/components/AppointmentRequests.js
+++ src/components/AppointmentRequests.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import moment from "moment"
+import { get } from 'lodash'
+import { useMutation, useQuery } from 'react-query'
+import toast, { Toaster } from 'react-hot-toast';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const AppointmentRequests = ({ date, timeSlot, appointment = [{ _id: 'ddd' },{ _id: 'ddd' }], onClickAccept }) => {
+import { getRequests, updateRequest } from 'libs/apis'
+
+const AppointmentRequests = ({ sellerId, timeSlotId, date, timeSlot }) => {
+  const { isFetching, error, refetch, data } = useQuery(["request", sellerId, timeSlotId],
+    () => getRequests(sellerId, timeSlotId)
+  )
+  const { mutate } = useMutation(({requestId, isAccepted}) => updateRequest(sellerId, timeSlotId, requestId, { isAccepted }), {
+    onSuccess: () => {
+      refetch()
+      toast.success('Appointment Request Accepted.')
+    },
+    onError: () => {
+      toast.error('Something Went Wrong.')
+    }
+  })
+  const requests = get(data, 'data', [])
   const readableDate = moment(date).format('MMMM Do')
 
   return (
@@ -13,10 +31,10 @@ const AppointmentRequests = ({ date, timeSlot, appointment = [{ _id: 'ddd' },{ _
         <h3> Appointment Requests for {readableDate}: {timeSlot[0]} - {timeSlot[1]} </h3>
         <List>
           {
-            appointment.map(({ _id, isAccepted }) => (
+            requests.map(({ _id, isAccepted }) => (
               <ListItem className='bg-color-1 mp-8' key={_id} autoFocus>
                 <ListItemText primary={_id} />
-                <button className='primary-btn bg-success' onClick={() => onClickAccept(isAccepted)}>{isAccepted ? 'reject' : 'accept'}</button>
+                <button className='primary-btn bg-success' onClick={() => mutate({requestId: _id, isAccepted: true })}>{isAccepted ? 'reject' : 'accept'}</button>
               </ListItem>
             ))
           }
--- src/components/TimeSlots.js
+++ src/components/TimeSlots.js
@@ -4,21 +4,23 @@ import { get } from 'lodash'
 const TimeSlots = ({ data, isLoading, error, onClickAdd, onClickSlot }) => {
     const slots = get(data, 'data.timeSlots', [])
 
-    const renderSingleSlot = (index, { start, end, isBooked, appointmentRequests }) => {
-        return (<div className='single-time-slot' onClick={() => { onClickSlot({ start, end, isBooked })}} >
-            <b className='single-time-slot-index'>#{index + 1}</b>
-            <div><span>{start}</span> - <span>{end}</span></div>
-            {
-                isBooked 
-                    ? <span className='single-time-slot-booked color-success'>booked</span>
-                    : appointmentRequests.length > 0 && <span className='single-time-slot-request-count bg-fail'>{appointmentRequests.length}</span>
-            }
-            
-        </div>)
+    const renderSingleSlot = (index, { _id, start, end, isBooked, appointmentRequests }) => {
+        return (
+            <div key={_id} className='single-time-slot' onClick={() => { onClickSlot({ _id, start, end, isBooked }) }} >
+                <b className='single-time-slot-index'>#{index + 1}</b>
+                <div><span>{start}</span> - <span>{end}</span></div>
+                {
+                    isBooked
+                        ? <span className='single-time-slot-booked color-success'>booked</span>
+                        : appointmentRequests.length > 0 && <span className='single-time-slot-request-count bg-fail'>{appointmentRequests.length}</span>
+                }
+
+            </div>
+        )
     }
 
     if (isLoading) return 'Loading...'
- 
+
     if (error) return 'An error has occurred: ' + error.message
 
     return (
